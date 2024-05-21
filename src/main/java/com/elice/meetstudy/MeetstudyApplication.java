a99549296e81e50d3efd373653b17d4612bb4492
[Cloud2] 이하영 <haileyeong@gmail.com>
2024-05-22 01:06:28 +0900

    Initial commit

---
+++ src/main/java/com/elice/meetstudy/MeetstudyApplication.java
@@ -0,0 +1,13 @@
+package com.elice.meetstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/meetstudy/MeetstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.meetstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
