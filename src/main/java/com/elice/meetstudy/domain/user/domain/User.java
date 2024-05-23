3d3789390a709849cd3885abf1e221d5731d0cd2
seoyeonkang <seoyeonkang512@gmail.com>
2024-05-23 23:05:26 +0900

    Feat: 회원가입 기능 구현

--- src/main/java/com/elice/meetstudy/domain/user/domain/User.java
+++ src/main/java/com/elice/meetstudy/domain/user/domain/User.java
@@ -3,10 +3,8 @@ package com.elice.meetstudy.domain.user.domain;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.context.annotation.Role;
+import lombok.*;
+import org.springframework.expression.spel.ast.NullLiteral;
 
 /**
  * user Entity
@@ -14,6 +12,7 @@ import org.springframework.context.annotation.Role;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
@@ -42,4 +41,20 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+
+    public void updateDeletedAt() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    @Builder
+    public User(String email, String password, String username, String nickname,
+                LocalDateTime joinAt, LocalDateTime deletedAt, Role role){
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.nickname = nickname;
+        this.joinAt = joinAt;
+        this.deletedAt = deletedAt;
+        this.role = role;
+    }
   }
