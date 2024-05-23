e1d87b126f174926b07182018ff93d8af2e34020
soo <yoons1609@gmail.com>
2024-05-23 20:55:43 +0900

    Feat: 기본 엔티티 추가

---
+++ src/main/java/com/elice/meetstudy/domain/calendar/domain/Calendar_detail.java
@@ -0,0 +1,65 @@
+package com.elice.meetstudy.domain.calendar.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Calendar_detail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "calendar_id")
+    private Calendar calendar;
+
+    @NotNull
+    private String title;
+
+    private String content;
+
+    @NotNull
+    private String start_day;
+
+    @NotNull
+    private String end_day;
+
+    @NotNull
+    private String start_time;
+
+    @NotNull
+    private String end_time;
+
+    @NotNull
+    private boolean isHoliday;
+
+    //메모 내용 빠진 일정 추가하기 위함
+    public Calendar_detail(Calendar calendar, String title, String start_day,
+        String end_day, String start_time, String end_time, boolean isHoliday) {
+        this.calendar = calendar;
+        this.title = title;
+        this.start_day = start_day;
+        this.end_day = end_day;
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.isHoliday = isHoliday;
+    }
+
+    public void setHoliday(boolean holiday) {
+        isHoliday = holiday;
+    }
+}
