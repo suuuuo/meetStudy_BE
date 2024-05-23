07a900a47a5da9e5217cf615b560ee40823ad6bc
seoyeonkang <seoyeonkang512@gmail.com>
2024-05-23 23:04:13 +0900

    Feat: my-page 초기 파일 세팅

---
+++ src/main/java/com/elice/meetstudy/domain/user/controller/MyPageController.java
@@ -0,0 +1,13 @@
+package com.elice.meetstudy.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/mypage")
+@RequiredArgsConstructor
+@Slf4j
+public class MyPageController {
+}
