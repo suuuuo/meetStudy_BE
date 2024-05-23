6fd8aed08cf763082291a55eb106cb43835305fb
soo <yoons1609@gmail.com>
2024-05-23 20:58:17 +0900

    Feat: 공휴일 정보 저장 기능 추가

---
+++ src/main/java/com/elice/meetstudy/domain/calendar/holiday/repository/HolidayRepository.java
@@ -0,0 +1,16 @@
+package com.elice.meetstudy.domain.calendar.holiday.repository;
+
+import com.elice.meetstudy.domain.calendar.holiday.domain.Holiday;
+import com.elice.meetstudy.domain.calendar.holiday.service.HolidayService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HolidayRepository extends JpaRepository<Holiday, Long> {
+    boolean existsByDate(String date);
+
+    Optional<Holiday> findByDate(String date);
+    List<Holiday> findByDateStartingWith(String datePrefix);
+}
