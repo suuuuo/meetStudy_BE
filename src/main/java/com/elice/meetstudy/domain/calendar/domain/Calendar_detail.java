78b87b3ea3a69ea15d7148e494b6c118910fe14b
soo <yoons1609@gmail.com>
2024-05-24 17:38:22 +0900

    Feat: 일정 엔티티 수정

--- src/main/java/com/elice/meetstudy/domain/calendar/domain/Calendar_detail.java
+++ src/main/java/com/elice/meetstudy/domain/calendar/domain/Calendar_detail.java
@@ -11,6 +11,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @Entity
 @Getter
@@ -33,31 +35,36 @@ public class Calendar_detail {
     private String content;
 
     @NotNull
-    private String start_day;
+    @JoinColumn(name = "start_day")
+    private String startDay;
 
     @NotNull
-    private String end_day;
+    @JoinColumn(name = "end_day")
+    private String endDay;
 
     @NotNull
-    private String start_time;
+    @JoinColumn(name = "start_time")
+    private String startDime;
 
     @NotNull
-    private String end_time;
+    @JoinColumn(name = "end_time")
+    private String endDime;
 
     @NotNull
+    @ColumnDefault("false")
     private boolean isHoliday;
 
     //메모 내용 빠진 일정 추가하기 위함
-    public Calendar_detail(Calendar calendar, String title, String start_day,
-        String end_day, String start_time, String end_time, boolean isHoliday) {
-        this.calendar = calendar;
-        this.title = title;
-        this.start_day = start_day;
-        this.end_day = end_day;
-        this.start_time = start_time;
-        this.end_time = end_time;
-        this.isHoliday = isHoliday;
-    }
+//    public Calendar_detail(Calendar calendar, String title, String start_day,
+//        String end_day, String start_time, String end_time, boolean isHoliday) {
+//        this.calendar = calendar;
+//        this.title = title;
+//        this.start_day = start_day;
+//        this.end_day = end_day;
+//        this.start_time = start_time;
+//        this.end_time = end_time;
+//        this.isHoliday = isHoliday;
+//    }
 
     public void setHoliday(boolean holiday) {
         isHoliday = holiday;
