7885353b171cff8c068a77e15ed45a795471e5b7
haileyeong <haileyeong@gmail.com>
2024-05-25 06:26:08 +0900

    Feat: 게시글 작성 기능 및 테스트를 위한 csrf 비활성화

--- src/main/java/com/elice/meetstudy/domain/post/domain/Post.java
+++ src/main/java/com/elice/meetstudy/domain/post/domain/Post.java
@@ -4,13 +4,18 @@ import com.elice.meetstudy.domain.category.entity.Category;
 import com.elice.meetstudy.domain.user.domain.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  *  post Entity
@@ -18,29 +23,47 @@ import lombok.Getter;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
-  @JoinColumn(name = "category_id", nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id")
   private Category category;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
   @Column(nullable = false)
   private String title;
 
+  //@Lob
   @Column(nullable = false)
   private String content;
 
-  @Column(nullable = false, columnDefinition = "bigint DEFAULT 0")
-  private Long hit;
+  @Column(nullable = false)
+  private Long hit = 0L;
 
-  @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  @Column(name = "created_at", nullable = false)
   private LocalDateTime createdAt;
+
+  @PrePersist
+  protected void onCreate() {
+    createdAt = LocalDateTime.now();
+  }
+
+
+  @Builder
+  public Post(Category category, User user, String title, String content) {
+    this.category = category;
+    this.user = user;
+    this.title = title;
+    this.content = content;
+  }
+
+
 }
