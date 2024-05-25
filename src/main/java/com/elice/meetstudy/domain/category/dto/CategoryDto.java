c6b15417b202cad9aafcf4599624bf0876d13853
blaumonde <larazili174@gmail.com>
2024-05-25 10:37:22 +0900

    Feat : annotation 추가

--- src/main/java/com/elice/meetstudy/domain/category/dto/CategoryDto.java
+++ src/main/java/com/elice/meetstudy/domain/category/dto/CategoryDto.java
@@ -2,11 +2,13 @@ package com.elice.meetstudy.domain.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class CategoryDto {
 
     @NotNull
