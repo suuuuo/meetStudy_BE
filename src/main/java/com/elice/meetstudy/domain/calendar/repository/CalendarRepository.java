96bfa99f733c330ee8367eee3ae1df19045dc40f
soo <yoons1609@gmail.com>
2024-05-26 16:19:16 +0900

    Feat: 개인 캘린더 조회 기능 수정

--- src/main/java/com/elice/meetstudy/domain/calendar/repository/CalendarRepository.java
+++ src/main/java/com/elice/meetstudy/domain/calendar/repository/CalendarRepository.java
@@ -4,17 +4,17 @@ import com.elice.meetstudy.domain.calendar.domain.Calendar;
 import com.elice.meetstudy.domain.calendar.domain.Calendar_detail;
 import java.util.List;
 import java.util.Optional;
+import org.mapstruct.ap.shaded.freemarker.template.utility.OptimizerUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+public interface
+CalendarRepository extends JpaRepository<Calendar, Long> {
 
+    //roomId가 없는, 개인 캘린더 찾기 : 스터디룸 아이디 없는 조건 추가해야함
     Optional<Calendar> findByUserId(Long userId);
 
-    //roomId가 없는, 개인 캘린더 찾기
-    Calendar findByUserIdAndRoomIdIsNull(Long userId);
-
     //roomId 있는, 공용 캘린더 찾기
-    Calendar findByRoomId(Long roomId);
+    //Optional<Calendar> findByroomId(Long roomId);
 }
