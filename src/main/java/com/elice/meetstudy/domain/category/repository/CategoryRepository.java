43f7b4044673882e665539c01a1a124939ec00b1
blaumonde <larazili174@gmail.com>
2024-05-28 00:23:36 +0900

    Feat : findById 메서드 추가

--- src/main/java/com/elice/meetstudy/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/elice/meetstudy/domain/category/repository/CategoryRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findByName(String categoryName);
+
+    Optional<Category> findById(Long id);
 }
