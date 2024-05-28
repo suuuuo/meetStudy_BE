7ec34cb3fad517033f6674674c88570d06a67352
seoyeonkang <seoyeonkang512@gmail.com>
2024-05-28 23:59:42 +0900

    Feat: 로그인 관련 security 설정

--- src/main/java/com/elice/meetstudy/config/SecurityConfig.java
+++ src/main/java/com/elice/meetstudy/config/SecurityConfig.java
@@ -1,21 +1,55 @@
 package com.elice.meetstudy.config;
 
+import com.elice.meetstudy.domain.user.jwt.JwtAccessDeniedHandler;
+import com.elice.meetstudy.domain.user.jwt.JwtAuthenticationEntryPoint;
+import com.elice.meetstudy.domain.user.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        // 해수함수를 이용하여 비밀번호를 암호화하여 저장.
-        return new BCryptPasswordEncoder();
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtFilter jwtFilter;
+    private final String[] adminUrl = {"api/vi/admin/**"};
+    private final String[] userUrl = {"api/vi/user/**"};
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws  Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(handle -> handle
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/vi/**").permitAll()
+                        .requestMatchers(adminUrl).hasRole("ADMIN")
+                        .requestMatchers(userUrl).hasRole("USER")
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
