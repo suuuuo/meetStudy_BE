59e564e88c0b0db3ef94f9a78bfc4ef0a8215b51
blaumonde <larazili174@gmail.com>
2024-05-31 21:48:36 +0900

    Refactor : Controller 코드 리팩토링

--- src/main/java/com/elice/meetstudy/domain/category/controller/CategoryController.java
+++ src/main/java/com/elice/meetstudy/domain/category/controller/CategoryController.java
@@ -1,11 +1,8 @@
 package com.elice.meetstudy.domain.category.controller;
 
-import com.elice.meetstudy.domain.category.dto.CategoryDto;
 import com.elice.meetstudy.domain.category.entity.Category;
 import com.elice.meetstudy.domain.category.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,38 +11,35 @@ import java.util.List;
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
 
-    @Autowired
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
-    // 카테고리 조회
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // 모든 카테고리 조회
     @GetMapping
     public ResponseEntity<List<Category>> getAllCategories() {
-        List<Category> categories = categoryService.findAllCategories();
-        return ResponseEntity.ok(categories);
+        return ResponseEntity.ok(categoryService.findAllCategories());
     }
 
     // id에 해당하는 카테고리 조회
     @GetMapping("/{id}")
     public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
-        Category category = categoryService.findCategory(id);
-        return ResponseEntity.ok(category);
+        return ResponseEntity.ok(categoryService.findCategory(id));
     }
 
     // 카테고리 생성
     @PostMapping("/add")
-    public ResponseEntity<Category> createCategory(@RequestBody CategoryDto categoryDto) {
-        Category category = new Category(categoryDto.getName(), categoryDto.getDescription());
-        Category createdCategory = categoryService.createCategory(category);
-        return ResponseEntity.ok(createdCategory);
+    public ResponseEntity<Category> createCategory(@RequestBody String name) {
+        return ResponseEntity.ok(categoryService.createCategory(name));
     }
 
     // 카테고리 수정
     @PutMapping("/{id}/edit")
-    public ResponseEntity<Category> updateCategory(@RequestBody CategoryDto categoryDto,
-                                                   @PathVariable Long id) {
-        Category updatedCategory = new Category(categoryDto.getName(), categoryDto.getDescription());
-        Category category = categoryService.updateCategory(updatedCategory, id);
-        return ResponseEntity.ok(category);
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id,
+                                                   @RequestBody String newName) {
+        return ResponseEntity.ok(categoryService.updateCategory(id, newName));
     }
 
     // 카테고리 삭제
