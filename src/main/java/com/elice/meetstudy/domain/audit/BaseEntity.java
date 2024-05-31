151744b65dd8913273020d4ef4ad475fcb970aae
soo <yoons1609@gmail.com>
2024-06-01 00:32:54 +0900

    Feat: auditing 추가

---
+++ src/main/java/com/elice/meetstudy/domain/audit/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.elice.meetstudy.domain.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
