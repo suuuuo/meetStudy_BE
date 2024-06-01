3e013efbd8649ac3309a0371799a1da8571cf25e
soo <yoons1609@gmail.com>
2024-06-01 18:25:10 +0900

    Feat: 접근 유저 정보 가져오게 변경

--- src/main/java/com/elice/meetstudy/domain/audit/AuditorAwareImpl.java
+++ src/main/java/com/elice/meetstudy/domain/audit/AuditorAwareImpl.java
@@ -1,15 +1,19 @@
 package com.elice.meetstudy.domain.audit;
 
+import com.elice.meetstudy.domain.user.domain.UserPrinciple;
 import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuditorAwareImpl implements AuditorAware<String> {
-
     @Override
     public Optional<String> getCurrentAuditor(){
-        /**
-         * 테스트를 위한 임시코드, jwt 토큰으로 아이디 가져오는 부분으로 대체해야함
-         */
-        return Optional.of("user");
+        //접근한 유저 정보 가져오는 로직
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        return Optional.of(userName);
     }
 }
