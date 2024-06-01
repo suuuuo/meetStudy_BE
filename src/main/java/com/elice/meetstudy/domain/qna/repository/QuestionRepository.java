7dcc97c41bceda9180d46ad026196267ab7bf1e5
soo <yoons1609@gmail.com>
2024-06-01 15:06:47 +0900

    Feat: 조회 페이징, 키워드 추가

--- src/main/java/com/elice/meetstudy/domain/qna/repository/QuestionRepository.java
+++ src/main/java/com/elice/meetstudy/domain/qna/repository/QuestionRepository.java
@@ -1,13 +1,16 @@
 package com.elice.meetstudy.domain.qna.repository;
 
 import com.elice.meetstudy.domain.qna.domain.Question;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    List<Question> findAllByOrderByIdDesc();
+    Page<Question> findAllByTitleContainingOrderByIdDesc(String title, Pageable pageable);
+
+    Page<Question> findAllByOrderByIdDesc(Pageable pageable);
 
 }
