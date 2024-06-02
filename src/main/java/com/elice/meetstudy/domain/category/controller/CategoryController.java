54920d75b3ad8ed4ca931804e22cf24276426661
blaumonde <larazili174@gmail.com>
2024-06-02 21:57:31 +0900

    Refactor : api 경로 수정

--- src/main/java/com/elice/meetstudy/domain/category/controller/CategoryController.java
+++ src/main/java/com/elice/meetstudy/domain/category/controller/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/categories")
+@RequestMapping("/api/v1/admin/categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
