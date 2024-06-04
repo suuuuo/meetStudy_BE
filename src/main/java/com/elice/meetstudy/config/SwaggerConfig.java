987cc244cd550b84a48430e21255be0eba84585a
haileyeong <haileyeong@gmail.com>
2024-06-05 02:33:53 +0900

    Chore: 불필요한 파일 삭제

--- src/main/java/com/elice/meetstudy/config/SwaggerConfig.java
+++ src/main/java/com/elice/meetstudy/config/SwaggerConfig.java
@@ -2,10 +2,6 @@ package com.elice.meetstudy.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -21,26 +17,11 @@ public class SwaggerConfig {
   @Bean
   public GroupedOpenApi chatOpenApi() {
     // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
-    String[] paths = {"/api/**"};
+    String[] paths = {"/**", "/api/user/**"};
 
     return GroupedOpenApi.builder()
         .group("MeetStudy API") // 그룹 이름 설정
         .pathsToMatch(paths) // 그룹에 속하는 경로 패턴 지정.
         .build();
-  }
-
-  @Bean
-  public OpenAPI api() {
-    SecurityScheme apiKey =
-        new SecurityScheme()
-            .type(SecurityScheme.Type.APIKEY)
-            .in(SecurityScheme.In.HEADER)
-            .name("Authorization");
-
-    SecurityRequirement securityRequirement = new SecurityRequirement().addList("Bearer Token");
-
-    return new OpenAPI()
-        .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
-        .addSecurityItem(securityRequirement);
   }
 }
