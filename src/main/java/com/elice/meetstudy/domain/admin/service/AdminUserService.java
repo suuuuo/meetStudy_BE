de038a27eea6f7ea243ee7c97caf5ebce6fb1f51
blaumonde <larazili174@gmail.com>
2024-06-04 17:27:13 +0900

    Feat : 회원 조회, 삭제

--- src/main/java/com/elice/meetstudy/domain/admin/service/AdminUserService.java
+++ src/main/java/com/elice/meetstudy/domain/admin/service/AdminUserService.java
@@ -1,13 +1,37 @@
 package com.elice.meetstudy.domain.admin.service;
 
+import com.elice.meetstudy.domain.user.domain.User;
 import com.elice.meetstudy.domain.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class AdminService {
+public class AdminUserService {
 
     private final UserRepository userRepository;
 
+    // 모든 회원 조회
+    public Page<User> findAllUsers(Pageable pageable) {
+        return userRepository.findByRole(List.of("USER"), pageable);
+    }
+
+    // id로 회원 조회
+    public User findUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    // 회원 삭제
+    public void deleteUser(Long userId) {
+        User foundUser = userRepository.findById(userId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        userRepository.delete(foundUser);
+    }
 }
