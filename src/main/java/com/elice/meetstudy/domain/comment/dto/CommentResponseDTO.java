d64fb20b0be345fb00ec7aa521c5e2888771a2bd
haileyeong <haileyeong@gmail.com>
2024-06-04 22:19:11 +0900

    Refactor: 게시글, 댓글 코드 수정

--- src/main/java/com/elice/meetstudy/domain/comment/dto/CommentResponseDTO.java
+++ src/main/java/com/elice/meetstudy/domain/comment/dto/CommentResponseDTO.java
@@ -1,19 +1,21 @@
 package com.elice.meetstudy.domain.comment.dto;
 
 import com.elice.meetstudy.domain.comment.domain.Comment;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
 public class CommentResponseDTO {
-  //  private Long id;
+
   private final String ninkname;
   private Long postId;
   private String content;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yy.MM.dd HH:mm")
   private LocalDateTime createdAt;
 
   public CommentResponseDTO(Comment comment) {
-    //    this.id = comment.getId();
     this.ninkname = comment.getUser().getNickname();
     this.postId = comment.getPost().getId();
     this.content = comment.getContent();
