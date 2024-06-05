042852819c3f5f49e7574ba10e7651f30844b5ee
soo <yoons1609@gmail.com>
2024-06-05 22:43:18 +0900

    Feat: url 일부 변경

--- src/main/java/com/elice/meetstudy/domain/qna/controller/AnswerController.java
+++ src/main/java/com/elice/meetstudy/domain/qna/controller/AnswerController.java
@@ -24,21 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/answer")
+@RequestMapping("/api")
 @Tag(name = "답변", description = "문의에 대한 답변 관련 API 입니다.")
 public class AnswerController {
 
   private final AnswerService answerService;
 
   @Operation(summary = "답변 조회", description = "답변 조회가 가능합니다.")
-  @GetMapping("")
-  public ResponseEntity<ResponseAnswerDto> getAnswer(@RequestParam long questionId) {
+  @GetMapping("answer/public/{questionId}")
+  public ResponseEntity<ResponseAnswerDto> getAnswer(@PathVariable long questionId) {
     return new ResponseEntity<>(answerService.getAnswer(questionId), HttpStatus.OK);
   }
 
   @Operation(summary = "답변 등록",
       description = "답변 등록이 가능합니다. 답변이 등록되면 질문의 상태가 답변 완료로 수정됩니다.")
-  @PostMapping("/{questionId}")
+  @PostMapping("admin/answer/{questionId}")
   public ResponseEntity<ResponseAnswerDto> addAnswer(
       @RequestBody @Valid RequestAnswerDto requestAnswerDto, @PathVariable long questionId)
       throws AccessDeniedException {
@@ -46,7 +46,7 @@ public class AnswerController {
   }
 
   @Operation(summary = "답변 수정", description = "답변 수정이 가능합니다.")
-  @PutMapping("/{answerId}")
+  @PutMapping("admin/answer/{answerId}")
   public ResponseEntity<ResponseAnswerDto> updateAnswer(
       @PathVariable long answerId, @RequestBody @Valid RequestAnswerDto requestAnswerDto)
       throws AccessDeniedException {
@@ -54,7 +54,7 @@ public class AnswerController {
   }
 
   @Operation(summary = "답변 삭제", description = "답변 삭제가 가능합니다. 질문 상태가 변경됩니다")
-  @DeleteMapping("/{answerId}")
+  @DeleteMapping("admin/answer/{answerId}")
   public ResponseEntity<ResponseAnswerDto> deleteAnswer(@PathVariable long answerId)
       throws AccessDeniedException {
      answerService.deleteAnswer(answerId);
--- src/main/java/com/elice/meetstudy/domain/qna/controller/QuestionController.java
+++ src/main/java/com/elice/meetstudy/domain/qna/controller/QuestionController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/question")
+@RequestMapping("api")
 @Tag(name = "문의", description = "문의 게시판 관련 API 입니다.")
 public class QuestionController {
 
@@ -35,7 +36,7 @@ public class QuestionController {
   /** 질문 전체 조회 (키워드) */
   @Operation(summary = "질문 조회",
       description = "키워드나 카테고리를 입력하면 해당되는 질문 조회, 입력하지 않으면 전체 질문 조회가 가능합니다.")
-  @GetMapping("")
+  @GetMapping("/question/public")
   public ResponseEntity<List<ResponseQuestionDto>> getAllQuestions(
       @RequestParam(defaultValue = "0") int page,
       @RequestParam(defaultValue = "20") int size,
@@ -50,7 +51,7 @@ public class QuestionController {
    */
   @Operation(summary = "질문 조회",
       description = "질문 id를 통해 개별 조회가 가능합니다.")
-  @GetMapping("/{questionId}")
+  @GetMapping("/question/public/{questionId}")
   public ResponseEntity<ResponseQuestionDto> getQuestion(@PathVariable long questionId,
       @RequestParam(required = false) String password) throws AccessDeniedException {
     return new ResponseEntity<>(questionService.getQuestion(questionId, password), HttpStatus.OK);
@@ -61,8 +62,8 @@ public class QuestionController {
    * @return responseQuestionDto 질문 생성
    */
   @Operation(summary = "질문 등록", description = "질문 등록이 가능합니다. 상태는 대기중으로 지정됩니다.")
-  @PostMapping("")
-  public ResponseEntity<ResponseQuestionDto> postQuestion(@Valid @RequestBody RequestQuestionDto requestQuestionDto) {
+  @PostMapping("/question")
+  public ResponseEntity<ResponseQuestionDto> postQuestion(@Validated @RequestBody RequestQuestionDto requestQuestionDto) {
     return new ResponseEntity<>(questionService.postQuestion(requestQuestionDto), HttpStatus.OK) ;
   }
 
@@ -73,7 +74,7 @@ public class QuestionController {
    * @return
    */
   @Operation(summary = "질문 수정", description = "질문 수정이 가능합니다.")
-  @PutMapping("/{questionId}")
+  @PutMapping("/question/{questionId}")
   public ResponseEntity<ResponseQuestionDto> updateQuestion(@RequestBody @Valid RequestQuestionDto requestQuestionDto,
       @PathVariable long questionId) throws AccessDeniedException {
     return new ResponseEntity<>(questionService.updateQuestion(requestQuestionDto, questionId),
@@ -84,7 +85,7 @@ public class QuestionController {
    * 질문 삭제
    */
   @Operation(summary = "질문 삭제", description = "질문 삭제가 가능합니다.")
-  @DeleteMapping("/{questionId}")
+  @DeleteMapping("/question/{questionId}")
   public ResponseEntity<HttpStatus> deleteQuestion(@PathVariable long questionId){
      questionService.deleteQuestion(questionId);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
