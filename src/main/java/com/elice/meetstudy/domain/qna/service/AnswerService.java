5d2c86abeb100b39b6c7540442e0c52b702677eb
soo <yoons1609@gmail.com>
2024-06-05 19:00:51 +0900

    Fix: 예외 처리 오류 수정

--- src/main/java/com/elice/meetstudy/domain/qna/service/AnswerService.java
+++ src/main/java/com/elice/meetstudy/domain/qna/service/AnswerService.java
@@ -8,16 +8,14 @@ import com.elice.meetstudy.domain.qna.dto.ResponseAnswerDto;
 import com.elice.meetstudy.domain.qna.mapper.AnswerMapper;
 import com.elice.meetstudy.domain.qna.repository.AnswerRepository;
 import com.elice.meetstudy.domain.qna.repository.QuestionRepository;
+import com.elice.meetstudy.domain.studyroom.exception.EntityNotFoundException;
 import com.elice.meetstudy.domain.user.domain.Role;
 import com.elice.meetstudy.domain.user.domain.User;
 import com.elice.meetstudy.domain.user.domain.UserPrinciple;
 import com.elice.meetstudy.domain.user.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import java.nio.file.AccessDeniedException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -35,15 +33,18 @@ public class AnswerService {
 
 
   /** 답변 조회 */
-  @Transactional
+
   public ResponseAnswerDto getAnswer(long questionId) {
     Optional<Question> question = questionRepository.findById(questionId);
+    System.out.println(question.get().getId());
     if (question.isPresent()) {
-      long answerId = question.get().getAnswer().getId();
-      if(answerId != 0L)
-      return answerMapper.toResponseAnswerDto(answerRepository.findById(answerId).get());
+      Answer answer = question.get().getAnswer();
+      if(answer == null || answer.getId() == null){
+        throw new EntityNotFoundException("답변이 등록되지 않았습니다.");
+      }
+      return answerMapper.toResponseAnswerDto(answerRepository.findById(answer.getId()).get());
     }
-    throw new EntityNotFoundException();
+    throw new NotFoundException("질문이 존재하지 않습니다.");
   }
 
   /** 답변 생성 */
@@ -60,7 +61,7 @@ public class AnswerService {
         question.get().setAnswer(answer);
         return answerMapper.toResponseAnswerDto(answerRepository.save(answer));
       }
-      throw new EntityNotFoundException();
+      throw new EntityNotFoundException("질문이 존재하지 않습니다.");
     }throw new AccessDeniedException("관리자만 접근 가능합니다.");
   }
 
@@ -75,8 +76,7 @@ public class AnswerService {
         Answer answer1 = answer.get();
         answer1.update(requestAnswerDto.content());
         return answerMapper.toResponseAnswerDto(answer1);
-      }
-      throw new NotFoundException(null);
+      } throw new EntityNotFoundException("존재하는 답변이 아닙니다.");
     } throw new AccessDeniedException("관리자만 접근 가능합니다.");
   }
 
