c4d92e6897aa070f88e9d627e482fca9d0a239f3
haileyeong <haileyeong@gmail.com>
2024-06-06 01:30:55 +0900

    Refactor: 토큰을 이용한 스크랩 기능으로 수정

--- src/main/java/com/elice/meetstudy/domain/scrap/dto/ScrapInsertDTO.java
+++ src/main/java/com/elice/meetstudy/domain/scrap/dto/ScrapInsertDTO.java
@@ -1,19 +1,29 @@
 package com.elice.meetstudy.domain.scrap.dto;
 
+import jakarta.validation.constraints.Null;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class ScrapInsertDTO {
 
-  private Long categoryId;
-  private Long postId;
-
+  @Null private Long categoryId;
+  @Null private Long postId;
 
   @Builder
   public ScrapInsertDTO(Long categoryId, Long postId) {
     this.categoryId = categoryId;
     this.postId = postId;
+    //    if (categoryId != null) {
+    //      this.categoryId = categoryId;
+    //    } else {
+    //      this.categoryId = null;
+    //    }
+    //
+    //    if (postId != null) {
+    //      this.postId = postId;
+    //    } else {
+    //      this.postId = null;
+    //    }
   }
-
 }
