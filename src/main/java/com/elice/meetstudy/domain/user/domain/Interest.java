6a1e8e83b4579520faff8905efa3719c7a495b77
seoyeonkang <seoyeonkang512@gmail.com>
2024-06-05 12:30:48 +0900

    Feat: 회원가입시 관심분야 선택 개발중

--- src/main/java/com/elice/meetstudy/domain/user/domain/Interest.java
+++ src/main/java/com/elice/meetstudy/domain/user/domain/Interest.java
@@ -17,11 +17,20 @@ public class Interest {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "category_id")
     private Category category;
 
-    public Interest(User user, Category category) {}
+    public static Interest createInterest(User user, Category category) {
+        Interest interest = new Interest();
+        interest.setUser(user);
+        interest.setCategory(category);
+        user.addInterest(interest); // User 클래스에 관심사를 추가하는 메소드를 호출합니다.
+        return interest;
+    }
+
+    private void setCategory(Category category) {
+    }
 
     public void setUser(User user) {} // 없으면 실행 안됨 -> 추가
 
