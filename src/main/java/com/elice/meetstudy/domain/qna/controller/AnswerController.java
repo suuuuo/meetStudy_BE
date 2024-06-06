71509bfce1f9240b7b148a7b328b6a91de134250
soo <yoons1609@gmail.com>
2024-06-06 22:27:11 +0900

    Refactor: 스웨거 설명 추가, api 오류 수정

--- src/main/java/com/elice/meetstudy/domain/qna/controller/AnswerController.java
+++ src/main/java/com/elice/meetstudy/domain/qna/controller/AnswerController.java
@@ -6,10 +6,10 @@ import com.elice.meetstudy.domain.qna.service.AnswerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.nio.file.AccessDeniedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,13 +28,14 @@ public class AnswerController {
   private final AnswerService answerService;
 
   @Operation(summary = "답변 조회", description = "답변 조회가 가능합니다.")
-  @GetMapping("answer/public/{questionId}")
+  @GetMapping("/answer/public/{questionId}")
   public ResponseEntity<ResponseAnswerDto> getAnswer(@PathVariable long questionId) {
     return new ResponseEntity<>(answerService.getAnswer(questionId), HttpStatus.OK);
   }
 
-  @Operation(summary = "답변 등록", description = "답변 등록이 가능합니다. 답변이 등록되면 질문의 상태가 답변 완료로 수정됩니다.")
-  @PostMapping("admin/answer/{questionId}")
+  @Operation(summary = "답변 등록(관리자)", description = "questionId로 질문을 찾아 답변을 등록합니다."
+      + " 답변이 등록되면 질문의 답변 상태가 PENDING 에서 COMPLETE 로 변경됩니다.")
+  @PostMapping("/admin/answer/{questionId}")
   public ResponseEntity<ResponseAnswerDto> addAnswer(
       @RequestBody @Valid RequestAnswerDto requestAnswerDto, @PathVariable long questionId)
       throws AccessDeniedException {
@@ -42,8 +43,8 @@ public class AnswerController {
         answerService.addAnswer(requestAnswerDto, questionId), HttpStatus.OK);
   }
 
-  @Operation(summary = "답변 수정", description = "답변 수정이 가능합니다.")
-  @PutMapping("admin/answer/{answerId}")
+  @Operation(summary = "답변 수정(관리자)", description = "answerId로 답변을 찾아 수정합니다.")
+  @PutMapping("/admin/answer/{answerId}")
   public ResponseEntity<ResponseAnswerDto> updateAnswer(
       @PathVariable long answerId, @RequestBody @Valid RequestAnswerDto requestAnswerDto)
       throws AccessDeniedException {
@@ -51,8 +52,9 @@ public class AnswerController {
         answerService.updateAnswer(requestAnswerDto, answerId), HttpStatus.OK);
   }
 
-  @Operation(summary = "답변 삭제", description = "답변 삭제가 가능합니다. 질문 상태가 변경됩니다")
-  @DeleteMapping("admin/answer/{answerId}")
+  @Operation(summary = "답변 삭제(관리자)", description = "answerId로 답변을 찾아 삭제합니다. "
+      + "질문 상태가 COMPLETE 에서 PENDING 으로 변경됩니다")
+  @DeleteMapping("/admin/answer/{answerId}")
   public ResponseEntity<ResponseAnswerDto> deleteAnswer(@PathVariable long answerId)
       throws AccessDeniedException {
     answerService.deleteAnswer(answerId);
