39cad68cab55c66ebc5fb1d0de23cfbe418a3098
haileyeong <haileyeong@gmail.com>
2024-06-07 16:17:31 +0900

    Feat: 게시글 좋아요 기능 작성.(좋아요, 취소, 좋아요 수 조회)

--- src/main/java/com/elice/meetstudy/domain/post/domain/PostLike.java
+++ src/main/java/com/elice/meetstudy/domain/post/domain/PostLike.java
@@ -3,9 +3,15 @@ package com.elice.meetstudy.domain.post.domain;
 import com.elice.meetstudy.domain.user.domain.User;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Table(name = "post_like")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostLike {
 
   @Id
@@ -26,5 +32,11 @@ public class PostLike {
   @PrePersist
   protected void onCreate() {
     createdAt = LocalDateTime.now();
+  }
+
+  @Builder
+  public PostLike(Post post, User user) {
+    this.post = post;
+    this.user = user;
   }
 }
