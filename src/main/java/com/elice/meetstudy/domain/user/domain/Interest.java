551a88879f80f4491b96e42bbb49d6da130ded41
seoyeonkang <seoyeonkang512@gmail.com>
2024-06-07 11:39:40 +0900

    Feat: 회원가입시 관심분야 선택 기능 추가, 회원 정보 수정에서 관심사 수정 추가

--- src/main/java/com/elice/meetstudy/domain/user/domain/Interest.java
+++ src/main/java/com/elice/meetstudy/domain/user/domain/Interest.java
@@ -4,34 +4,24 @@ import com.elice.meetstudy.domain.category.entity.Category;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Interest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
     @OneToOne
     @JoinColumn(name = "category_id")
     private Category category;
 
-    public static Interest createInterest(User user, Category category) {
+    public static Interest createInterest(Category category) {
         Interest interest = new Interest();
-        interest.setUser(user);
         interest.setCategory(category);
-        user.addInterest(interest); // User 클래스에 관심사를 추가하는 메소드를 호출합니다.
         return interest;
     }
-
-    private void setCategory(Category category) {
-    }
-
-    public void setUser(User user) {} // 없으면 실행 안됨 -> 추가
-
 }
