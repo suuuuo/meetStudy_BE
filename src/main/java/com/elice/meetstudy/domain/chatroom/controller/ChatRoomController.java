9e1f6567b78e6d167c72e0327495281dee5135a1
YAE-Joon <hyjoon93@gmail.com>
2024-06-07 17:47:46 +0900

    feat : STOMP에 JWT 검증기능 구현

--- src/main/java/com/elice/meetstudy/domain/chatroom/controller/ChatRoomController.java
+++ src/main/java/com/elice/meetstudy/domain/chatroom/controller/ChatRoomController.java
@@ -26,33 +26,33 @@ public class ChatRoomController {
 
   @Autowired private ChatRoomService chatRoomService;
 
-  @Operation(summary = "채팅방 생성")
+  @Operation(summary = "채팅방 생성", description = "스터디룸id를 받아와서 채팅방을 생성합니다.")
   @PostMapping("/chatroom/add")
   public ResponseEntity<CreateChatRoomDto> createChatRoom(
       @RequestBody CreateChatRoomDto requestChatRoomDto) {
     return ResponseEntity.ok(chatRoomService.createchatRoom(requestChatRoomDto));
   }
 
-  @Operation(summary = "채팅방 조회")
+  @Operation(summary = "채팅방 조회", description = "채팅방 정보를 조회합니다.")
   @GetMapping("/chatroom/{chatRoomId}")
   public ResponseEntity<ChatRoomDto> chatRoomDto(@PathVariable Long chatRoomId) {
     return ResponseEntity.ok(chatRoomService.findByChatRoomId(chatRoomId));
   }
 
-  @Operation(summary = "채팅방 삭제")
+  @Operation(summary = "채팅방 삭제", description = "채팅방을 삭제합니다.")
   @DeleteMapping("/chatroom/{chatRoomId}")
   public ResponseEntity<Void> deleteChatroom(@PathVariable Long chatRoomId) {
     chatRoomService.deleteChatRoom(chatRoomId);
     return ResponseEntity.noContent().build();
   }
 
-  @Operation(summary = "스터디룸에 해당하는 채팅방 리스트 불러오기")
+  @Operation(summary = "스터디룸에 해당하는 채팅방 리스트 불러오기", description = "스터디룸id를 받아서 해당하는 모든 채팅방을 조회합니다.")
   @GetMapping("/studyrooms/{studyRoomId}/chatroom")
   public ResponseEntity<List<ChatRoomDto>> chatRoomList(@PathVariable Long studyRoomId) {
     return ResponseEntity.ok(chatRoomService.chatRoomList(studyRoomId));
   }
 
-  @Operation(summary = "공지사항 수정")
+  @Operation(summary = "공지사항 수정", description = "공지사항을 수정힙니다.")
   @PutMapping("/chatroom/notice")
   public ResponseEntity<ChatRoomDto> createNotice(@RequestBody ChatRoomDto chatRoomDto) {
     return ResponseEntity.ok(chatRoomService.createNotice(chatRoomDto));
