17f04ed01a72b80a5d3bdc2aba7d0298ad7a7170
YAE-Joon <hyjoon93@gmail.com>
2024-06-10 22:56:54 +0900

    feat : 접속한 시점부터 이전 메세지 보이는 기능 구현

--- src/main/java/com/elice/meetstudy/domain/chatroom/repository/MessageRepository.java
+++ src/main/java/com/elice/meetstudy/domain/chatroom/repository/MessageRepository.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
 
-  @Query("SELECT m FROM Message m JOIN FETCH m.sender s JOIN FETCH m.chatRoom c WHERE c.id = :chatRoomId")
-  Page<Message> findMessagesWithChatRoomAndUsers(@Param("chatRoomId") Long chatRoomId, Pageable pageable);
+  @Query("SELECT m FROM Message m "
+      + "JOIN FETCH m.sender s "
+      + "JOIN FETCH m.chatRoom c "
+      + "WHERE c.id = :chatRoomId "
+      + "and m.id >= (select MIN(m2.id) from Message "
+      + "m2 where m2.sender.id= :senderId)")
+  Page<Message> findMessagesWithChatRoomAndUsers(@Param("chatRoomId") Long chatRoomId, @Param("senderId") Long senderId, Pageable pageable);
 }
