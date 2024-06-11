d840dc71b1e4b35075118b60cd9052b28065b398
YAE-Joon <hyjoon93@gmail.com>
2024-06-11 22:34:10 +0900

    fix : 메세지 페이징 방식변경

--- src/main/java/com/elice/meetstudy/domain/chatroom/controller/ChatController.java
+++ src/main/java/com/elice/meetstudy/domain/chatroom/controller/ChatController.java
@@ -8,8 +8,6 @@ import com.elice.meetstudy.domain.user.jwt.token.TokenProvider;
 import com.elice.meetstudy.util.EntityFinder;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.security.Principal;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -19,17 +17,11 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
