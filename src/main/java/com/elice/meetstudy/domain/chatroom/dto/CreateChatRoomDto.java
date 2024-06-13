e1ee5bb5d29bcccbe5bc7497d623c5269924ffc6
YAE-Joon <hyjoon93@gmail.com>
2024-06-14 03:16:48 +0900

    fix : chatRoomDto, message Jpql 수정

--- src/main/java/com/elice/meetstudy/domain/chatroom/dto/CreateChatRoomDto.java
+++ src/main/java/com/elice/meetstudy/domain/chatroom/dto/CreateChatRoomDto.java
@@ -8,13 +8,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class CreateChatRoomDto {
-
+    private Long chatRoomId;
     private String title;
     private Long studyRoomId;
     private String notice;
 
   @Builder
   public CreateChatRoomDto(ChatRoom chatRoom){
+    this.chatRoomId = chatRoom.getId();
     this.title = chatRoom.getTitle();
     this.studyRoomId = chatRoom.getStudyRoom().getId();
     this.notice = chatRoom.getNotice();
